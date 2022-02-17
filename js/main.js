dd05e6c300d17b28d412c9c359ca5531819ce271
Robert <quizzez1912@gmail.com>
2022-02-17 19:57:25 +0100

    my projects rdy

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-function scrollTo(divID){
-    console.log("Scroll To: " + divID);
-    var element = document.getElementById(divID);
+
+function scrollToProject(name){
+    console.log("Scroll To: Project ==  " + name);
+    var element = document.getElementById(name);
     element.scrollIntoView();
 }
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  
